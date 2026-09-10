0dbf50c98e05fd7b9ca6311b9c2136d43b041f74
EbnulAhsan <ebnu1721@gmail.com>
2026-09-11 02:08:58 +0600

    refreshToken api done

---
+++ service/refreshToken.ts
@@ -0,0 +1,39 @@
+"use server"
+
+import { cookies } from "next/headers";
+
+export const refreshToken = async () => {
+    const cookieStore = await cookies();
+
+    const refreshToken = cookieStore.get("accessToken")?.value || null;
+
+    if (!refreshToken) {
+        // throw new Error("User Not Logged In!");
+
+        return {
+            success: false,
+            message: "User not logged in!"
+        }
+    }
+
+    const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/refreshToken`, {
+        headers: {
+            // Authorization : accessToken as unknown as string,
+            // Authorization : `${accessToken}`,
+            // Authorization : `Bearer ${accessToken}`
+
+            Cookie: `accessToken=${refreshToken}`
+        },
+
+        cache: "no-store",
+        // next: {
+        //     revalidate: 60 * 60 * 24, // 1day
+        //     tags: ["my-profile"]
+        // }
+    });
+
+    const result = res.json();
+
+
+    return result
+}
